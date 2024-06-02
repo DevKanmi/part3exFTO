ab03da318db747507e688f5d7a197a86f7c695fa
Akinbayo Clinton <clintexakinbs@gmail.com>
2024-06-02 05:20:02 +0100

    update

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-const http = require("http")
+const express = require("express")
 const app = express()
+
+
+app.get("/" ,(request, response) =>{
+    response.send('<h1>hello world</h1>')
+})
+
+app.post("/",(req,res)=>{
+    res.send("Testing 1....2...")
+})
+const PORT = "3001"
+app.listen(PORT)
+console.log('Server is listening on port ${PORT}')
